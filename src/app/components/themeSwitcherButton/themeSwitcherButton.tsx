518151914586c8e1932da8662053363bd4134f63
phyo thiha <phyothiha97531@gmail.com>
2024-10-24 13:12:08 +0630

    fix error

--- src/app/components/themeSwitcherButton/themeSwitcherButton.tsx
+++ src/app/components/themeSwitcherButton/themeSwitcherButton.tsx
@@ -2,7 +2,6 @@
 
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const ThemeSwitcherButton = () => {
