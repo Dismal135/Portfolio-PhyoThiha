df77a277d96ba3569dac6576727103aa04f67bbe
unknown <phyothiha97531@gmail.com>
2024-10-30 19:40:29 +0630

    'fix'

--- src/app/components/projects/projects.tsx
+++ src/app/components/projects/projects.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import quoteMachine from "@/app/images/quotemachine.png"
 import documentation from "@/app/images/technicaldoc.png"
-import product from "@/app/images/productlandingpage.png"
+import product from "@/app/images/productlandingpage-sandy.vercel.app_ (1).png"
 import tictactoe from "@/app/images/reacttictactoe.png"
 import job from '@/app/images/joblandingweb.png'
 
@@ -10,7 +10,7 @@ const Projects = () => {
         <section id="projects" className="mt-10 flex flex-wrap gap-10 items-center justify-items-center justify-center">
             <figure className="inline-block w-fit">
                 <Image src={quoteMachine} alt="random quote machine" width={500} height={500} />
-                <a target="_blank" href="https://randomquotemachinephyothiha.netlify.app"><figcaption className="text-center hover:underline" >Random Quote Machine</figcaption></a>
+                <a target="_blank" href="https://quote-machine-drab-xi.vercel.app"><figcaption className="text-center hover:underline" >Random Quote Machine</figcaption></a>
                 </figure>
                 <figure className="inline-block w-fit">
                 <Image src={job} alt="job landing website" width={500} height={500} />
@@ -18,15 +18,15 @@ const Projects = () => {
                 </figure>
                 <figure className="inline-block w-fit">
                 <Image src={documentation} alt="react technical documentation" width={500} height={500} />
-                <a target="_blank" href="https://reacttechnicaldocumentation.netlify.app/"><figcaption className="text-center hover:underline" >React Techinical Documentation</figcaption></a>
+                <a target="_blank" href="https://technical-documentation-eta.vercel.app"><figcaption className="text-center hover:underline" >React Techinical Documentation</figcaption></a>
                 </figure>
                 <figure className="inline-block w-fit">
                 <Image src={tictactoe} alt="tic tac toe game" width={500} height={500} />
-                <a target="_blank" href="https://reacttictactoepth.netlify.app/"><figcaption className="text-center hover:underline" >React TicTacToe</figcaption></a>
+                <a target="_blank" href="https://react-tic-tac-toe-ten-green.vercel.app"><figcaption className="text-center hover:underline" >React TicTacToe</figcaption></a>
                 </figure>
                 <figure className="inline-block w-fit">
                 <Image src={product} alt="product landing page" width={500} height={500} />
-                <a target="_blank" href="https://productlandingpagephyothiha.netlify.app/"><figcaption className="text-center hover:underline" >Product Landing Page</figcaption></a>
+                <a target="_blank" href="https://productlandingpage-sandy.vercel.app"><figcaption className="text-center hover:underline" >Responsive Website</figcaption></a>
                 </figure>
                 <div className="lg:block hidden w-[500px]"></div>
         </section>
