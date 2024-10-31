5bd2b11db67b8f18318fe1f96ea5c020da0a3d46
unknown <phyothiha97531@gmail.com>
2024-10-31 15:30:20 +0630

    'fix'

--- src/app/components/projects/projects.tsx
+++ src/app/components/projects/projects.tsx
@@ -10,23 +10,23 @@ const Projects = () => {
         <section id="projects" className="mt-10 flex flex-wrap gap-10 items-center justify-items-center justify-center">
             <figure className="inline-block w-fit">
                 <Image src={quoteMachine} alt="random quote machine" width={500} height={500} />
-                <a target="_blank" href="https://quote-machine-drab-xi.vercel.app"><figcaption className="text-center hover:underline" >Random Quote Machine</figcaption></a>
+                <a className="underline" target="_blank" href="https://quote-machine-drab-xi.vercel.app"><figcaption className="text-center hover:underline" >Random Quote Machine</figcaption></a>
                 </figure>
                 <figure className="inline-block w-fit">
                 <Image src={job} alt="job landing website" width={500} height={500} />
-                <a target="_blank" href="https://joblandingwebsite.vercel.app/"><figcaption className="text-center hover:underline" >Websites Showcase</figcaption></a>
+                <a className="underline" target="_blank" href="https://joblandingwebsite.vercel.app/"><figcaption className="text-center hover:underline" >Websites Showcase</figcaption></a>
                 </figure>
                 <figure className="inline-block w-fit">
                 <Image src={documentation} alt="react technical documentation" width={500} height={500} />
-                <a target="_blank" href="https://technical-documentation-eta.vercel.app"><figcaption className="text-center hover:underline" >React Techinical Documentation</figcaption></a>
+                <a className="underline" target="_blank" href="https://technical-documentation-eta.vercel.app"><figcaption className="text-center hover:underline" >React Techinical Documentation</figcaption></a>
                 </figure>
                 <figure className="inline-block w-fit">
                 <Image src={tictactoe} alt="tic tac toe game" width={500} height={500} />
-                <a target="_blank" href="https://react-tic-tac-toe-ten-green.vercel.app"><figcaption className="text-center hover:underline" >React TicTacToe</figcaption></a>
+                <a className="underline" target="_blank" href="https://react-tic-tac-toe-ten-green.vercel.app"><figcaption className="text-center hover:underline" >React TicTacToe</figcaption></a>
                 </figure>
                 <figure className="inline-block w-fit">
                 <Image src={product} alt="product landing page" width={500} height={500} />
-                <a target="_blank" href="https://productlandingpage-sandy.vercel.app"><figcaption className="text-center hover:underline" >Responsive Website</figcaption></a>
+                <a className="underline" target="_blank" href="https://productlandingpage-sandy.vercel.app"><figcaption className="text-center hover:underline" >Responsive Website</figcaption></a>
                 </figure>
                 <div className="lg:block hidden w-[500px]"></div>
         </section>
