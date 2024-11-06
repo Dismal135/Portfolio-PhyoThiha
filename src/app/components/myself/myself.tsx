2a2e54cd14fad16efffb7b5de6da469ec321a22c
PHYO THI HA <phyothiha97531@gmail.com>
2024-11-06 21:22:20 +0630

    myself

--- src/app/components/myself/myself.tsx
+++ src/app/components/myself/myself.tsx
@@ -2,7 +2,7 @@ const Myself = () => {
     return (
         <div className="md:w-1/2">
             <h1><strong>About me</strong></h1>
-            <p className="text-sm text-justify">Hello! My name is Phyo Thi Ha. I was born on November 24th 1996 in Myanmar Yangon. I graduated from No.5 Education High School BoTaHtaung. My current education status is Dagon University English Major and learning programming at FreeCodeCamp. I want to become a webdeveloper because I like to work as a webdeveloper in my future company. Please check my portfolio and contact me if you are interested or need more informations.</p>
+            <p className="text-sm text-justify">Hello! My name is Phyo Thi Ha. I was born on November 24th 1996 in Myanmar Yangon. I am learning programming at FreeCodeCamp. I want to become a webdeveloper because I want to improve my life by solving problems and challenging things. Please check my portfolio and contact me if you are interested or need more informations.</p>
         </div>
     )
 };
